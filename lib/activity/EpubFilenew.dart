89fee1377458713b84dbf633b26195e545dc993f
gaurav raj <gaurav.raj@doors4s.com>
2021-01-20 19:25:20 +0530

    pdftron added

--- lib/activity/EpubFilenew.dart
+++ lib/activity/EpubFilenew.dart
@@ -17,6 +17,8 @@ import 'package:flutterapp/utils/utils.dart';
 import 'package:nb_utils/nb_utils.dart';
 import '../main.dart';
 import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:pdftron_flutter/pdftron_flutter.dart';
 
 // import 'package:open_file/open_file.dart';
 // import 'package:dio/dio.dart';
@@ -50,20 +52,100 @@ class ViewEPubFileNewState extends State<ViewEPubFileNew> {
   DownloadedBook mSampleDownloadTask;
   DownloadedBook mBookDownloadTask;
   int currentPage = 0;
-
-
-
+  bool _showViewer = true;
+  String _document =
+      "https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf";
+  String _version = 'Unknown';
+  String filePath;
   @override
   void initState() {
     super.initState();
     initialDownload();
-    getBookmark();
+    initPlatformState();
+    showViewer();
+  /*  getBookmark();*/
+  }
+  Future<void> initPlatformState() async {
+    String version;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      PdftronFlutter.initialize("your_pdftron_license_key");
+      version = await PdftronFlutter.version;
+    } on PlatformException {
+      version = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _version = version;
+    });
   }
 
+  void showViewer() async {
+    // opening without a config file will have all functionality enabled.
+    // await PdftronFlutter.openDocument(_document);
+
+    // shows how to disale functionality
+//      var disabledElements = [Buttons.shareButton, Buttons.searchButton];
+//      var disabledTools = [Tools.annotationCreateLine, Tools.annotationCreateRectangle];
+    var config = Config();
+//      config.disabledElements = disabledElements;
+//      config.disabledTools = disabledTools;
+//      config.multiTabEnabled = true;
+//      config.customHeaders = {'headerName': 'headerValue'};
+
+    var documentLoadedCancel = startDocumentLoadedListener((filePath) {
+      print("document loaded: $filePath");
+    });
+
+    await PdftronFlutter.openDocument(_document, config: config);
+
+    try {
+      PdftronFlutter.importAnnotationCommand(
+          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+              "    <xfdf xmlns=\"http://ns.adobe.com/xfdf/\" xml:space=\"preserve\">\n" +
+              "      <add>\n" +
+              "        <square style=\"solid\" width=\"5\" color=\"#E44234\" opacity=\"1\" creationdate=\"D:20200619203211Z\" flags=\"print\" date=\"D:20200619203211Z\" name=\"c684da06-12d2-4ccd-9361-0a1bf2e089e3\" page=\"1\" rect=\"113.312,277.056,235.43,350.173\" title=\"\" />\n" +
+              "      </add>\n" +
+              "      <modify />\n" +
+              "      <delete />\n" +
+              "      <pdf-info import-version=\"3\" version=\"2\" xmlns=\"http://www.pdftron.com/pdfinfo\" />\n" +
+              "    </xfdf>");
+    } on PlatformException catch (e) {
+      print("Failed to importAnnotationCommand '${e.message}'.");
+    }
+
+    try {
+      PdftronFlutter.importBookmarkJson('{"0":"Page 1"}');
+    } on PlatformException catch (e) {
+      print("Failed to importBookmarkJson '${e.message}'.");
+    }
+
+    var annotCancel = startExportAnnotationCommandListener((xfdfCommand) {
+      // local annotation changed
+      // upload XFDF command to server here
+      print("flutter xfdfCommand: $xfdfCommand");
+    });
+
+    var bookmarkCancel = startExportBookmarkListener((bookmarkJson) {
+      print("flutter bookmark: $bookmarkJson");
+    });
+
+    var path = await PdftronFlutter.saveDocument();
+    print("flutter save: $path");
+
+    // to cancel event:
+    // annotCancel();
+    // bookmarkCancel();
+  }
   // ignore: missing_return
   Future initialDownload() async {
     if (widget._isFileExist) {
-      String filePath =
+      filePath =
           await getBookFilePath(widget.mBookId, widget.downloads.file);
       setState(() {
         isDownloadFile = true;
@@ -219,7 +301,7 @@ class ViewEPubFileNewState extends State<ViewEPubFileNew> {
             // for pdf
             : !widget.isPDFFile
                 ? SizedBox()
-                : Container(
+                : /*Container(
                     height: MediaQuery.of(context).size.height * 0.85,
                     child: PDFView(
                       filePath: fullFilePath,
@@ -234,13 +316,26 @@ class ViewEPubFileNewState extends State<ViewEPubFileNew> {
                       },
                       defaultPage: currentPage,
                     ),
-                  ),
+                  ),*/
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          child:
+          // Uncomment this to use Widget version of the viewer
+          _showViewer
+              ? DocumentView(
+            onCreated: _onDocumentViewCreated,
+          ):
+          Container(),
+        ),
+
+
       ),
 
       //bookmark page
-      floatingActionButton: FloatingActionButton(
+   /*   floatingActionButton: FloatingActionButton(
         backgroundColor: appStore.scaffoldBackground,
-        /*child: ,*/
+        *//*child: ,*//*
         child: pressed ? Text(widget.bookMark,style: TextStyle(color: Colors.black),):Icon(Icons.bookmark_border_sharp),
         onPressed: () {
 
@@ -252,10 +347,10 @@ class ViewEPubFileNewState extends State<ViewEPubFileNew> {
           print('${widget.bookMark} page no bookmark Saved');
           print('$pressed bool value bookmark Saved');
         },
-      ),
+      ),*/
     );
   }
-   saveBookmark() async {
+/*   saveBookmark() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString('pageBookmark',widget.bookMark );
     prefs.setString('bookId', widget.mBookId );
@@ -271,8 +366,45 @@ class ViewEPubFileNewState extends State<ViewEPubFileNew> {
     print('${widget.bookMark} prefs value----> bookmark Saved');
     print('${widget.mBookId}  prefs BOOk Id ----> bookmark Saved');
     print('$pressed prefs bool value----> bookmark Saved');
-  }
+  }*/
+  void _onDocumentViewCreated(DocumentViewController controller) async {
+    Config config = new Config();
 
+    var leadingNavCancel = startLeadingNavButtonPressedListener(() {
+      // Uncomment this to quit the viewer when leading navigation button is pressed
+      this.setState(() {
+        _showViewer = !_showViewer;
+      });
+
+      // Show a dialog when leading navigation button is pressed
+      _showMyDialog();
+    });
+
+    controller.openDocument(_document, config: config);
+  }
+  Future<void> _showMyDialog() async {
+    print('hello');
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog'),
+          content: SingleChildScrollView(
+            child: Text('Leading navigation button has been pressed.'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
   void _resumeDownload(_TaskInfo task) async {
     String newTaskId = await FlutterDownloader.resume(taskId: task.taskId);
     task.taskId = newTaskId;
